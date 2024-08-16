7c8528a9d8765eee80ae2110de9e221f0d294a17
sumeetkbedi <sumeet.bedi@library.ca.gov>
2024-08-15 17:11:47 -0700

    Modified contract district check part 1 code to read in error contracts from error data folder, and put in commented code that act as a "check" to see if any changes actually come from this step. was not needed for 2024 MEIS

--- src/check_contract_district_part1.R
+++ src/check_contract_district_part1.R
@@ -1,6 +1,6 @@
 ##PART 1: Check if contracts without a district value actually have one in the cleaned data##
 #Read in needed contract files - the 1 missing only districts, the main errors file, and the cleaned file
-error_contracts <- read.csv(file.path(output_path, paste0(f_year, contract_errors)))
+error_contracts <- read.csv(file.path(err_check_path, paste0(f_year, contract_errors)))
 clean_contracts <- read.csv(file.path(temp_path, paste0(f_year, clean_c_data)))
 
 #Make a list of unique recipient names and zip codes from contract_errors
@@ -20,3 +20,21 @@ for(i in 1:nrow(error_cont_biz)) {
     error_contracts$recipient_congressional_district[k] <- district[1]
   }
 }
+
+
+#error_cont_biz <- unique(data.frame(recipient_name = error_contracts$recipient_name,
+#                                    recipient_zip_4_code = error_contracts$recipient_zip_4_code,
+#                                    cd = error_contracts$recipient_congressional_district))
+
+#clean_cont_biz <- unique(data.frame(recipient_name = clean_contracts$recipient_name,
+#                                    recipient_zip_4_code = clean_contracts$recipient_zip_4_code,
+#                                    cd = clean_contracts$recipient_congressional_district))
+
+#cont_merged <- merge(error_cont_biz, clean_cont_biz, by.x = c("recipient_name", "recipient_zip_4_code"),
+#                     by.y = c("recipient_name", "recipient_zip_4_code"), all = T)
+
+#cont_merged <- cont_merged %>%
+#  filter((cont_merged$cd.x == 90))# %>%
+  #select(!(cd.x))
+
+#write.csv(cont_merged, file.path(err_check_path, "combined_contract_biz.csv"), row.names = F)
